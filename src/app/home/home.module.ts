e858deea11e81c3796c366221076bfc70f9a2e5c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-10-28 23:24:47 -0300

    feature: Created the home screen

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './index';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HomeComponent
+  ],
+})
+export class HomeModule { }
